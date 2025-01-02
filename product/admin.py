8aaa42da2727beede8f3c79ee819bef88c5314d1
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2025-01-02 22:31:38 +0330

    Comments were placed in the product app files.

--- product/admin.py
+++ product/admin.py
@@ -3,26 +3,35 @@ from .models import Product, ProductImage
 from django import forms
 
 
+# Inline model to manage product images directly from the product admin page.
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
+    # Number of empty forms displayed for adding new images.
     extra = 1
+    # Makes the image preview field read-only.
     readonly_fields = ('image_preview',)
     fields = ('image_preview', 'image', 'order')
+    # Orders images by their 'order' field in ascending order.
     ordering = ('order',)
 
 
+# Custom form for the Product model to provide additional configurations for form fields.
 class ProductAdminForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
         widgets = {
+            # Customizes the 'price' field to have a step size of 10,000 and a default value of 10,000.
             'price': forms.NumberInput(attrs={'step': 10000, 'value': 10000, }),
+            # Customizes the 'stock' field to have a default value of 10.
             'stock': forms.NumberInput(attrs={'value': 10, }),
         }
 
 
+# Custom admin configuration for the Product model to enhance functionality in the admin panel.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     form = ProductAdminForm
     list_display = ('name', 'stock', 'price')
+    # Adds an inline interface to manage associated ProductImage instances.
     inlines = [ProductImageInline]
--- product/models.py
+++ product/models.py
@@ -7,7 +7,8 @@ from django.utils.html import mark_safe
 
 import os
 from uuid import uuid4
-from PIL import Image, ImageOps
+from PIL.ImageOps import expand as expand_image_PIL
+from PIL.Image import open as image_open_PIL
 
 
 class Product(models.Model):
@@ -19,31 +20,39 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
+    # ForeignKey linking this image to a product
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
     image_upload_to = "product_images/"
     image = models.ImageField(upload_to=image_upload_to)
     order = models.PositiveIntegerField(default=0)
 
     class Meta:
+        # Default ordering by the 'order' field
         ordering = ['order']
 
     def save(self, *args, **kwargs):
         def rename_product_image():
+            # Generate a unique filename for the image
             filename = str(uuid4())
             filename += str(self.image.width) + "_" + str(self.image.height) + "-" + str(self.image.width)
             filename = f"{filename}.png"
             self.image.name = filename
             print(filename)
 
+            # Save the instance
             super(self.__class__, self).save(*args, **kwargs)
 
+            # Construct the file path and open the image
             filename = str(settings.MEDIA_ROOT) + "/" + str(self._meta.get_field('image').upload_to) + filename
             print(filename)
-            img = Image.open(filename)
+            img = image_open_PIL(filename)
+
+            # Convert image to RGBA
             if img.mode != "RGBA":
                 img = img.convert("RGBA")
             width, height = img.size
 
+            # Calculate padding to make the image square
             if height > width:
                 padding = int((height - width) / 2)
                 padding = (padding, 0, padding, 0)
@@ -53,21 +62,25 @@ class ProductImage(models.Model):
             else:
                 padding = (0, 0, 0, 0)
 
-            img = ImageOps.expand(img, padding, (255, 255, 255, 0))
+            # Add padding to make the image square
+            img = expand_image_PIL(img, padding, (255, 255, 255, 0))
             img.save(filename, format="PNG")
 
         if self.pk:
+            # Handle existing image replacement
             old_image = ProductImage.objects.get(pk=self.pk).image
             if not old_image == self.image:
                 if os.path.isfile(old_image.path):
                     os.remove(old_image.path)
                     rename_product_image()
             else:
+                # Handle new image upload
                 super().save(*args, **kwargs)
 
         else:
             rename_product_image()
 
+    # Override delete method to remove the associated image file from storage.
     def delete(self, *args, **kwargs):
         if self.image:
             image_path = os.path.join(settings.MEDIA_ROOT, self.image.name)
@@ -75,6 +88,7 @@ class ProductImage(models.Model):
                 os.remove(image_path)
         super().delete(*args, **kwargs)
 
+    # Generate an HTML preview of the image for use in the admin panel.
     def image_preview(self):
         if self.image:
             return mark_safe(
@@ -84,6 +98,7 @@ class ProductImage(models.Model):
     image_preview.short_description = "Preview"
 
 
+# Signal to delete the image file from storage when a ProductImage instance is deleted.
 @receiver(post_delete, sender=ProductImage)
 def delete_product_image_file(sender, instance, **kwargs):
     if instance.image:
@@ -91,11 +106,12 @@ def delete_product_image_file(sender, instance, **kwargs):
             default_storage.delete(instance.image.name)
 
 
+# Signal to set the order of the image automatically based on the last image of the product.
 @receiver(pre_save, sender=ProductImage)
 def set_image_order(sender, instance, **kwargs):
-    if not instance.order:  # اگر order مشخص نباشد
+    if not instance.order:
         last_order = ProductImage.objects.filter(product=instance.product).order_by('order').last()
         if last_order:
-            instance.order = last_order.order + 1  # افزایش مقدار order از عدد آخر
+            instance.order = last_order.order + 1
         else:
-            instance.order = 1  # اگر هیچ عکسی وجود نداشت،
+            instance.order = 1
