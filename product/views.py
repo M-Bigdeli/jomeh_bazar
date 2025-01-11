62f00f0ef74eeca672aa9ea611fff7645cb37ea5
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2025-01-11 12:58:28 +0330

    Sort products by user based on newest, cheapest and most expensive. Create a product availability condition to display it on the product page (stock >= 1).

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.template import loader
@@ -15,16 +16,24 @@ def products(request, category_slug=None):
         except Category.DoesNotExist:
             return redirect(reverse("product:products"))
         context['category'] = category
-        paginator = Paginator(Product.objects.filter(category=category), 24)
+        products = Product.objects.filter(category=category)
         context['category_ancestors'] = category.get_ancestors(include_self=True)
         context['category_ancestors'].reverse()
         context['category_siblings'] = category.get_siblings(include_self=True)
         context['category_children'] = Category.objects.filter(parent=category)
     else:
-        paginator = Paginator(Product.objects.all(), 24)
+        products = Product.objects.all()
         context['category_siblings'] = Category.objects.filter(parent=None)
 
-    page = request.GET.get('page', 1)
+    if request.GET.get('in_stock'):
+        products = products.filter(stock__gte=1)
 
-    context['page_obj'] = paginator.get_page(page)
+    sort_by = request.GET.get('sort_by')
+    if sort_by == "cheapest":
+        products = products.order_by('price')
+    elif sort_by == "most_expensive":
+        products = products.order_by('-price')
+
+    page = request.GET.get('page', 1)
+    context['page_obj'] = Paginator(products, 24).get_page(page)
     return render(request, "product/product.html", context)
